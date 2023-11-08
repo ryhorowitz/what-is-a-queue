2527185702566e8997f23a522cbdc22afd5eb0d6
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-11-08 11:26:18 -0500

    complete

--- javascript/queue.js
+++ javascript/queue.js
@@ -9,43 +9,47 @@ class Queue {
   // add item to rear of queue if not full
   // if full throw error
   enqueue(item) {
+    if (this.isFull()) {
+      throw new Error("Queue is full")
+    }
 
+    this.queue.push(item)
   }
 
   // remove item from front of queue and return it
   dequeue() {
-
+    return this.queue.shift()
   }
 
   // return item at front of queue without removing it
   peek() {
-
+    return this.queue[0]
   }
 
   // return true if queue is empty, otherwise false
   isEmpty() {
-
+    return this.size() === 0
   }
 
   // return true if queue is full, otherwise false
   isFull() {
-
+    return this.size() >= this.limit
   }
 
   // return number of items in queue
   size() {
-
+    return this.queue.length
   }
 
   // return -1 if item not in queue, otherwise integer representing 
   // how far it is from the front
   search(target) {
-
+    return this.queue.indexOf(target)
   }
 
   // print contents of queue: do not return the queue itself!
   print() {
-    
+
   }
 }
 
